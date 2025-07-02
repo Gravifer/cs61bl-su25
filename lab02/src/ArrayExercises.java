f7f84dcb6c740d79bdd83dc9a48595b30d25be77
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-07-02 10:22:56 +0800

    lab02: complete array exercises (task 3)

--- lab02/src/ArrayExercises.java
+++ lab02/src/ArrayExercises.java
@@ -5,15 +5,24 @@ public class ArrayExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] values = new int[] { 1, 2, 3, 4, 5, 6 };
+        return values;
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max - min;
     }
 
 }
