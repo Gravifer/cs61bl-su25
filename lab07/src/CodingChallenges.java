1acbd9ffcde324848c07478630b436a59d1ceb16
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-07-03 22:20:29 +0800

    lab07: complete coding challenges

--- lab07/src/CodingChallenges.java
+++ lab07/src/CodingChallenges.java
@@ -12,8 +12,26 @@ public class CodingChallenges {
      * values from 0 to N except for one missing number.
      */
     public static int missingNumber(int[] values) {
-        // TODO
-        return -1;
+        // the array may not be sorted and may contain duplicates.
+        Set<Integer> seen = new HashSet<>();
+        int max = 0;
+        for (int value : values) {
+            if (value < 0) {
+                continue; // ignore negative numbers
+            }
+            seen.add(value);
+            if (value > max) {
+                max = value;
+            }
+        }
+        // Check for the missing number in the range 0 to max
+        for (int i = 0; i <= max; i++) {
+            if (!seen.contains(i)) {
+                return i;
+            }
+        }
+        // If all numbers from 0 to max are present, the missing number is max + 1
+        return max + 1;
     }
 
     /**
@@ -21,7 +39,23 @@ public class CodingChallenges {
      * permutation of s2 if it has the same number of each character as s2.
      */
     public static boolean isPermutation(String s1, String s2) {
-        // TODO
-        return false;
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s2.toCharArray()) {
+            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+                return false;
+            }
+            charCount.put(c, charCount.get(c) - 1);
+        }
+
+        return true;
     }
 }
--- lab07/test/CodingChallengesTest.java
+++ lab07/test/CodingChallengesTest.java
@@ -6,11 +6,42 @@ public class CodingChallengesTest {
 
     @Test
     public void testMissingNumber() {
-	// TODO
+	    // the missing number from an array of length N containing all the values from 0 to N except for one missing number.
+        // the array may not be sorted and may contain duplicates.
+        assertWithMessage("Missing number in [0] should be 1")
+                .that(CodingChallenges.missingNumber(new int[]{0})).isEqualTo(1);
+        assertWithMessage("Missing number in [0, 1] should be 2")
+                .that(CodingChallenges.missingNumber(new int[]{0, 1})).isEqualTo(2);
+        assertWithMessage("Missing number in [1] should be 0")
+                .that(CodingChallenges.missingNumber(new int[]{1})).isEqualTo(0);
+        assertWithMessage("Missing number in [0, 2] should be 1")
+                .that(CodingChallenges.missingNumber(new int[]{2, 0})).isEqualTo(1);
+        assertWithMessage("Missing number in [0, 1, 2, 3, 4] should be 5")
+                .that(CodingChallenges.missingNumber(new int[]{0, 1, 2, 3, 4})).isEqualTo(5);
+        assertWithMessage("Missing number in [0, 1, 2, 3, 5] should be 4")
+                .that(CodingChallenges.missingNumber(new int[]{0, 1, 2, 3, 5})).isEqualTo(4);
+        assertWithMessage("Missing number in [0, 1, 2, 3, 4, 5] should be 6")
+                .that(CodingChallenges.missingNumber(new int[]{0, 1, 2, 3, 4, 5})).isEqualTo(6);
+        assertWithMessage("Missing number in [1, 2, 3, 4, 5] should be 0")
+                .that(CodingChallenges.missingNumber(new int[]{1, 2, 3, 4, 5})).isEqualTo(0);
+        assertWithMessage("Missing number in [0, 1, 2, 4, 5, 6] should be 3")
+                .that(CodingChallenges.missingNumber(new int[]{4, 2, 0, 1, 6, 5})).isEqualTo(3);
     }
 
     @Test
     public void testIsPermutation() {
-	// TODO
+	    // true if and only if s1 is a permutation of s2. s1 is a permutation of s2 if it has the same number of each character as s2.
+        assertWithMessage("Empty strings should be permutations of each other")
+                .that(CodingChallenges.isPermutation("", "")).isTrue();
+        assertWithMessage("a should not be a permutation of b")
+                .that(CodingChallenges.isPermutation("a", "b")).isFalse();
+        assertWithMessage("abc should be a permutation of cba")
+                .that(CodingChallenges.isPermutation("abc", "cba")).isTrue();
+        assertWithMessage("abc should not be a permutation of abcd")
+                .that(CodingChallenges.isPermutation("abc", "abcd")).isFalse();
+        assertWithMessage("aabbcc should be a permutation of bbacca")
+                .that(CodingChallenges.isPermutation("aabbcc", "bbacca")).isTrue();
+        assertWithMessage("aabbcc should not be a permutation of aabbc")
+                .that(CodingChallenges.isPermutation("aabbcc", "aabbc")).isFalse();
     }
 }
