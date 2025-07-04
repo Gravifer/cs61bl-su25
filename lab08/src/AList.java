1eb863ccc491c2bc813527e1ee37ed7e227f07b1
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-07-04 17:37:05 +0800

    lab08: complete `AList` exercise

--- lab08/src/AList.java
+++ lab08/src/AList.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * An AList is a list of integers. Like SLList, it also hides the terrible
  * truth of the nakedness within, but uses an array as its base.
  */
-public class AList<Item> {
-
+public class AList<Item> implements Iterable<Item> {
     /* TODO: Make AList able to be iterated over. Add new nested classes as necessary.
     *   Your code will likely not compile on the autograder unless you implement this section.*/
 
@@ -13,6 +14,7 @@ public class AList<Item> {
     private int size;
 
     /** Creates an empty AList. */
+    @SuppressWarnings("unchecked")
     public AList() {
         // The line below gives a warning (Unchecked cast), but you can ignore this.
         items = (Item[]) new Object[8];
@@ -20,6 +22,7 @@ public class AList<Item> {
     }
 
     /** Returns a AList consisting of the given values. */
+    @SafeVarargs
     public static <Item> AList<Item> of(Item... values) {
         AList<Item> list = new AList<>();
         for (Item value : values) {
@@ -43,6 +46,7 @@ public class AList<Item> {
     }
 
     /** Resize the array to accommodate more items. */
+    @SuppressWarnings("unchecked")
     private void resize() {
         Item[] newItems = (Item[]) new Object[items.length * 2];
         System.arraycopy(items, 0, newItems, 0, size);
@@ -52,25 +56,31 @@ public class AList<Item> {
     /** Returns the representation of the AList as a String. */
     @Override
     public String toString() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int p = 0;
         boolean first = true;
         while (p != size) {
             if (first) {
-                result += items[p].toString();
+                result.append(items[p].toString());
                 first = false;
             } else {
-                result += " " + items[p].toString();
+                result.append(" ").append(items[p].toString());
             }
             p += 1;
         }
-        return result;
+        return result.toString();
     }
 
 
     /** Returns whether this and the given list or object are equal. */
     public boolean equals(Object o) {
-        AList other = (AList) o;
+        if (o instanceof AList<?> other) {
+            if (this.size != other.size) {
+                return false;
+            }
+        } else {
+            return false;
+        }
         return Arrays.deepEquals(items, other.getItems());
     }
 
@@ -79,4 +89,46 @@ public class AList<Item> {
         return items;
     }
 
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public Iterator<Item> iterator() {
+        return new AListIterator();
+    }
+
+    private class AListIterator implements Iterator<Item> {
+        private int currentIndex = 0;
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more elements in the AList iterator.");
+            }
+            Item item = items[currentIndex];
+            currentIndex++;
+            return item;
+        }
+    }
 }
